5b3681b4c3267448b26d3b5659b0583fbcea70e5
Nikita <wolfnikita11@gmail.com>
2026-06-18 18:51:08 +0300

    chore: initialize project infrastructure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <main className="flex min-h-screen items-center justify-center bg-slate-100">
+          <h1 className="text-5xl font-bold text-slate-800">
+              Currency Wallet
+          </h1>
+      </main>
+  );
+}
+
+export default App;
